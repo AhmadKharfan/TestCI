e9673df11a35346882343b6b268fd52de1ea7be6
ahmad <ahmad@test.com>
2025-05-07 12:11:37 +0300

    make tests

--- src/test/kotlin/TestClassTest.kt
+++ src/test/kotlin/TestClassTest.kt
@@ -25,23 +25,4 @@ class TestClassTest {
         val expected = (instance.x * 10) * instance.x
         assertEquals(expected.toString(), printed)
     }
-
-
-    @Test
-    fun `hello should print expected result`() {
-        // Arrange
-        val instance = TestClass()
-        val outputStream = ByteArrayOutputStream()
-        val originalOut = System.out
-        System.setOut(PrintStream(outputStream))
-
-        // Act
-        instance.hello()
-
-        // Assert
-        System.setOut(originalOut)
-        val printed = "hello world hahahah"
-        val expected = "hello"
-        assertEquals(expected, printed)
-    }
 }
